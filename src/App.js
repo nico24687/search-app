9c7d89937d5bca88703770cc46218209c7baade6
nico24687 <theonlynico@hotmail.com>
2018-04-08 18:10:49 -0600

    search form now displaying

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Grid, Jumbotron} from 'react-bootstrap'
+import SearchForm from './components/SearchForm'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
           <Grid>
             <h1>Search App</h1>
             <p>This is a simple search app</p>
+            <SearchForm/>
           </Grid>
         </Jumbotron>;
       </div>
